99ae038b634f15ac3f13751058b593c6ae8e8d15
nitzanto <nitzan16001@gmail.com>
2023-09-17 22:26:12 +0300

    * Migration changes

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -1,15 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+import { Student } from './src/students/entities/student.entity';
+import { Course } from './src/courses/entities/course.entity';
 
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+config();
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+const configService = new ConfigService();
+
+export default new DataSource({
     type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: 'nitzan123',
-    database: 'task',
-    entities: ['src/**/*.entity{.ts,.js}'],
-    synchronize: false,
-    migrationsTableName: 'migration',
-    migrations: ['src/migrations/*.ts'],
-};
+    host: configService.getOrThrow('MYSQL_HOST'),
+    port: configService.getOrThrow('MYSQL_PORT'),
+    database: configService.getOrThrow('MYSQL_DATABASE'),
+    username: configService.getOrThrow('MYSQL_USERNAME'),
+    password: configService.getOrThrow('MYSQL_PASSWORD'),
+    migrations: ['migrations/**'],
+    entities: [Student, Course],
+});
